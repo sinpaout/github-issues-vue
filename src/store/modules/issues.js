a21b7e86a05260c67f5d9ede0eee74e2bf7bb029
Thet Win Aung <minthit21dj@gmail.com>
2019-03-16 22:19:56 +0900

    Update vuex modules

--- src/store/modules/issues.js
+++ src/store/modules/issues.js
@@ -1,23 +1,48 @@
+import api from '../../service/api'
+
 const state = {
   items: [],
+  issues: null,
 }
 
 // getters
 const getters = {
   all: (state) => {
     return state.items.map(item => item)
+  },
+  allIssues: (state) => {
+    return state.issues && state.issues.filter(issue => !issue.pull_request) || []
+  },
+  openIssues: (state, getters) => {
+    return getters.allIssues.filter(issue => issue.state === 'open')
+  },
+  closedIssues: (state, getters) => {
+    return getters.allIssues.filter(issue => issue.state === 'closed')
   }
 }
 
 // actions
 const actions = {
+  loadIssues ({commit}) {
+    api.get('repos/sinpaout/first-github/issues?state=all').then((result) => {
+      commit('setIssues', result)
+    })
+  },
   addItem ({ commit }) {
     commit('pushItem', {time: Date.now()})
+  },
+  updateState ({ dispatch }, {number, state}) {
+    api.patch(`repos/sinpaout/first-github/issues/${number}`, {state}).then(() => {
+      dispatch('loadIssues')
+    })
   }
 }
 
 // mutations
 const mutations = {
+  setIssues (state, payload) {
+    state.issues = payload
+  },
   pushItem (state, item) {
     state.items.push(item)
   }
--- src/store/modules/page.js
+++ src/store/modules/page.js
@@ -1,11 +1,15 @@
 const state = {
   current: '',
+  loadings: 0
 }
 
 // getters
 const getters = {
   currentPage: (state) => {
     return state.current
+  },
+  isLoading: (state) => {
+    return state.loadings > 0
   }
 }
 
@@ -13,6 +17,15 @@ const getters = {
 const actions = {
   setCurrentRoute ({ commit }, route) {
     commit('setCurrentRouteName', route.name)
+  },
+  startRequest ({commit}, config) {
+    commit('showIndicator', config)
+  },
+  endRequest ({commit}, response) {
+    commit('hideIndicator', response.config)
+  },
+  errorRequest ({commit}, error) {
+    // commit('ajaxError', error)
   }
 }
 
@@ -20,7 +33,23 @@ const actions = {
 const mutations = {
   setCurrentRouteName (state, routeName) {
     state.current = routeName
+  },
+  showIndicator (state) {
+    state.loadings = state.loadings + 1
+  },
+  hideIndicator (state) {
+    state.loadings = state.loadings - 1
+  },
+  ajaxError (state, error = {}) {
+    let errorStatus
+    if (error && error.response) {
+      errorStatus = error.response.status
+    }
+    state.error = {
+      status: errorStatus
+    }
   }
+
 }
 
 export default {
